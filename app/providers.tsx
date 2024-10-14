9deeac2b38249acf8d2549f29abc4fdd1edb5ad9
n0nnnnc <napatchirawichada@gmail.com>
2024-10-14 05:07:04 -0700

    Clerk Authentication

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,9 +1,10 @@
 'use client'
 import { ThemeProvider } from './theme-provider'
-
+import { Toaster } from '@/components/ui/toaster'
 function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Toaster />
       <ThemeProvider
         attribute="class"
         defaultTheme="system"
