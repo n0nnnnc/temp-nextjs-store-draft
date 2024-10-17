2057eee9dde51f4978463d41263225a2178dee1e
n0nnnnc <napatchirawichada@gmail.com>
2024-10-17 05:15:05 -0700

    Upload Image to Supabase Bucket

--- utils/actions.ts
+++ utils/actions.ts
@@ -4,6 +4,7 @@ import db from '@/utils/db'
 import { currentUser } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
 import { imageSchema, productSchema, validateWithZodSchema } from './schemas'
+import { uploadImage } from './supabase'
 
 const getAuthUser = async () => {
   const user = await currentUser()
@@ -60,18 +61,18 @@ export const createProductAction = async (
     const rawData = Object.fromEntries(formData)
     const file = formData.get('image') as File
     const validateFields = validateWithZodSchema(productSchema, rawData)
-    const validateFile = validateWithZodSchema(imageSchema, { image: file })
-    console.log(validateFile)
+    const validatedFile = validateWithZodSchema(imageSchema, { image: file })
+    const fullPath = await uploadImage(validatedFile.image)
+
     await db.product.create({
       data: {
         ...validateFields,
-        image: '/images/product-3.jpg',
+        image: fullPath,
         clerkId: user.id,
       },
     })
-
-    return { message: 'product created' }
   } catch (error) {
     return renderError(error)
   }
+  redirect('/admin/products')
 }
